b802602b201088fd6326d4af1f7ef66c5ef384e0
Sahil Mahajan <sahilm754@gmail.com>
2020-08-29 09:26:16 -0400

    Strings
    Program checks if user given string is a Palindrome or not.

---
+++ Problem-18-Strings/palindromeCheck/palindrome.c
@@ -0,0 +1,33 @@
+//Palindrome
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	char str[10];
+	int len;
+	int start =0;
+	int end;
+	printf("Enter the string: ");
+	scanf("%s",str);
+
+	len = strlen(str);
+	end = len-1;
+
+	int result =0;
+	while(start <= end){
+		if(str[start] != str[end]){
+			result =1;
+			break;
+		}
+		start++;
+		end--;
+	}
+
+	(result ==0)?printf("Given string is palindrome"):printf("Given string is not pellindrome");
+
+	printf("\n");
+
+	return 0;
+}
