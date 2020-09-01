f99ac5f307065f3cb6f2b4ac6c0a6ec3d71d8e0d
Sahil M <sahilm@Sahils-MacBook-Pro.local>
2020-08-31 20:05:19 -0400

    Structures
    Added Structures folder and created blank student program project in it.

---
+++ Problem-19-Structures/student/student/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  student
+//
+//  Created by Sahil M on 2020-08-31.
+//  Copyright © 2020 Sahil. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
